285301f297c8476b876a7c11331eed86b150a5ed
Dream High <smartcoder0415@gmail.com>
2022-03-07 23:03:54 -0500

    done with scafolding

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,7 @@ import './styles/global.css';
 import { useEffect } from 'react';
 
 function App() {
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
-  };
-
   useEffect(() => {
-    // This is how we read messages sent from the plugin controller
     window.onmessage = (event) => {
       const { type, message } = event.data.pluginMessage;
       if (type === 'create-rectangles') {
@@ -19,8 +14,32 @@ function App() {
 
   return (
     <div>
-      <h2 className="text-3xl font-bold underline">Rectangle Creator</h2>
-      <button onClick={onCancel}>Cancel</button>
+      <div className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">Ready to dive in?</span>
+            <span className="block text-indigo-600">Hello world</span>
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Get started
+              </a>
+            </div>
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
