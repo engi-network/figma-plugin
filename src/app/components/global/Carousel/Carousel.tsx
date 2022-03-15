591c199eb0a1d43eb11d6df8b9bb6e655145ba7f
Dream High <smartcoder0415@gmail.com>
2022-03-15 13:15:03 -0400

    added some css

--- src/app/components/global/Carousel/Carousel.tsx
+++ src/app/components/global/Carousel/Carousel.tsx
@@ -13,12 +13,16 @@ function Carousel() {
       onSlideChange={() => console.info('slide change')}
       onSwiper={(swiper) => console.info(swiper)}
       className="w-full"
+      css={{ border: '1px solid gray', padding: '10px' }}
     >
       {Array(50)
         .fill(0)
         .map((_, index) => (
           <SwiperSlide key={index}>
-            <div className="border-dotted border-2 border-indigo-600">
+            <div
+              css={{ border: '1px solid gray' }}
+              className="border-dotted border-2 border-indigo-600"
+            >
               Slide {index}
             </div>
           </SwiperSlide>
