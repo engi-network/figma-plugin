93835db700a8d03a8bbdee726a617858beebd021
Dream High <smartcoder0415@gmail.com>
2022-03-14 20:39:50 -0400

    locked Node to 16.x current stable

--- src/app/components/global/Carousel/Carousel.tsx
+++ src/app/components/global/Carousel/Carousel.tsx
@@ -18,6 +18,8 @@ function Carousel() {
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
     </Swiper>
   )
 }
