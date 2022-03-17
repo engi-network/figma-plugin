e9450fe94b94387c00122ca154c933c504d2b04d
Dream High <smartcoder0415@gmail.com>
2022-03-17 19:48:07 -0400

    removed unnessary dependency

--- src/app/components/modules/Navigation/Navigation.tsx
+++ src/app/components/modules/Navigation/Navigation.tsx
@@ -1,13 +1,7 @@
 import Carousel from '~/app/components/global/Carousel/Carousel'
-// import Swiper from '~/app/components/global/Swiper/Swiper'
 
 function Navigation() {
-  return (
-    <>
-      <Carousel />
-      {/* <Swiper /> */}
-    </>
-  )
+  return <Carousel />
 }
 
 export default Navigation
