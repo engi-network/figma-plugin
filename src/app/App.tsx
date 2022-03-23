0b18bd2e88c8d6ffa122e67333e87d1dc711f0aa
Dream High <smartcoder0415@gmail.com>
2022-03-23 10:57:05 -0400

    api client

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,23 @@
 import './styles/global.css'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { MemoryRouter as Router } from 'react-router-dom'
 
 import Layout from '~/app/components/modules/App/Layout/Layout'
 import Routes from '~/app/pages/Routes'
 
+const queryClient = new QueryClient()
 function App() {
   return (
-    <Router initialEntries={['/', '/result']} initialIndex={0}>
-      <Layout>
-        <Routes />
-      </Layout>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router initialEntries={['/', '/result']} initialIndex={0}>
+        <Layout>
+          <Routes />
+        </Layout>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
