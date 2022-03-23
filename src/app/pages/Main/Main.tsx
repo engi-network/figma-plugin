887ce7104b05def06c23670e52781fa506751397
Dream High <smartcoder0415@gmail.com>
2022-03-22 20:06:26 -0400

    completed the create ui

--- src/app/pages/Main/Main.tsx
+++ src/app/pages/Main/Main.tsx
@@ -1,6 +1,7 @@
-import { ReplyIcon, XIcon } from '@heroicons/react/solid'
+import { PlusIcon, ReplyIcon } from '@heroicons/react/solid'
 import { useState } from 'react'
 
+import Button from '~/app/components/global/Button/Button'
 import IconButton from '~/app/components/global/IconButton/IconButton'
 import Code, { AnalyzeFormValues } from '~/app/components/modules/Code/Code'
 import Preview from '~/app/components/modules/Preview/Preview'
@@ -14,13 +15,17 @@ function Main() {
     setValues(values)
   }
 
+  const handleSubmit = () => {
+    console.info('submitting=====>', values)
+  }
+
   return (
     <>
       <div className="flex justify-between mb-10">
         <h1 css={styles.title}>{ui('main.title')}</h1>
         <div className="flex justify-center items-center">
           <IconButton
-            renderIcon={() => <XIcon className="w-5 h-5" />}
+            renderIcon={() => <PlusIcon className="w-5 h-5" />}
             className="bg-wf-light rounded-full w-8 h-8 mr-5"
           />
           <IconButton
@@ -29,7 +34,7 @@ function Main() {
           />
         </div>
       </div>
-      <div className="flex flex-1">
+      <div className="flex mb-10">
         <section className="w-6/12 border-solid border-r border-wf-tertiery">
           <Preview />
         </section>
@@ -37,7 +42,19 @@ function Main() {
           <Code onChange={handleChange} values={values} />
         </section>
       </div>
-      <div className="flex">footer</div>
+      <div className="flex justify-center mb-10">
+        <Button primary onClick={handleSubmit} className="w-5/12">
+          Analyze
+        </Button>
+      </div>
+      <footer className="flex justify-center">
+        <a href="/" className="flex items-center">
+          <span className="mr-6 text-sm text-wf-secondary">
+            {ui('header.learnMore')}
+            <span className="text-wf-primary">{ui('header.companyName')}</span>
+          </span>
+        </a>
+      </footer>
     </>
   )
 }
