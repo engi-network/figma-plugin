04435f3b4c67c5e58c10669703268fcb1cc41a92
Dream High <smartcoder0415@gmail.com>
2022-03-24 09:56:31 -0400

    removed accesskeym

--- src/app/lib/awsClients/index.ts
+++ src/app/lib/awsClients/index.ts
@@ -1,14 +1,18 @@
 import { SNSClient } from '@aws-sdk/client-sns'
+import AWS from 'aws-sdk'
 import S3 from 'aws-sdk/clients/s3'
 
 import config from '~/app/lib/config'
 
+AWS.config.update({
+  region: config.AWS_DEFAULT_REGION,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: config.IDENTITY_POOL_ID,
+  }),
+})
+
 export const snsClient = new SNSClient({
   region: config.AWS_DEFAULT_REGION,
-  credentials: {
-    accessKeyId: config.AWS_ACCESS_KEY_ID,
-    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
-  },
 })
 
 export const s3Client = new S3()
--- src/app/lib/config/index.ts
+++ src/app/lib/config/index.ts
@@ -1,9 +1,8 @@
 const config = {
-  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID || '',
   AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION || '',
-  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY || '',
   SAME_STORY_BUCKET_NAME: process.env.SAME_STORY_BUCKET_NAME || '',
   TOPIC_ARN: process.env.TOPIC_ARN || '',
+  IDENTITY_POOL_ID: process.env.IDENTITY_POOL_ID || '',
 }
 
 export default config
