fa475fb4d0ec33f41b724c8fa2b41bbc6831a07c
Dream High <smartcoder0415@gmail.com>
2022-04-14 19:51:44 -0400

    changed routes name in routes

--- src/app/pages/Routes.tsx
+++ src/app/pages/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { ROUTES, ROUTES_MAP } from '~/app/lib/constants'
 import HistoryPage from '~/app/pages/History/History.container'
 import LoadingPage from '~/app/pages/Loading/Loading'
 import MainPage from '~/app/pages/Main/Main.container'
@@ -8,10 +9,10 @@ import ResultPage from '~/app/pages/Result/Result.container'
 function RoutesPages() {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/result" element={<ResultPage />} />
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/loading" element={<LoadingPage />} />
+      <Route path={ROUTES_MAP[ROUTES.HOME]} element={<MainPage />} />
+      <Route path={ROUTES_MAP[ROUTES.RESULT]} element={<ResultPage />} />
+      <Route path={ROUTES_MAP[ROUTES.HISTORY]} element={<HistoryPage />} />
+      <Route path={ROUTES_MAP[ROUTES.LOADING]} element={<LoadingPage />} />
     </Routes>
   )
 }
