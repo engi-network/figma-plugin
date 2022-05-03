3cd0b472eb6c27aaa22b5027f1d6b5f0ec2300e4
Dream High <smartcoder0415@gmail.com>
2022-05-02 23:54:11 -0400

    added snapshot testing for codeblock

---
+++ src/app/components/global/CodeBlock/CodeBlock.test.tsx
@@ -0,0 +1,17 @@
+import renderer from 'react-test-renderer'
+
+import CodeBlock from './CodeBlock'
+
+const codeString = `const TestCompenent = () => {
+  return <div>Test</div>
+}
+`
+
+describe('CodeBlock', () => {
+  test('shoould render correctly', () => {
+    const tree = renderer
+      .create(<CodeBlock codeString={codeString} className={'w-36 h-20'} />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
