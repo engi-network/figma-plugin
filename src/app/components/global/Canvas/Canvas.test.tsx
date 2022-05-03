95679ddc20ab3b6cfa7f01ec599cd0c5cbb4987c
Dream High <smartcoder0415@gmail.com>
2022-05-02 21:28:35 -0400

    added canvas container test

--- src/app/components/global/Canvas/Canvas.test.tsx
+++ src/app/components/global/Canvas/Canvas.test.tsx
@@ -1,8 +1,9 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { CanvasOption } from '~/app/hooks/useCanvas'
 
 import Canvas from './Canvas'
+import CanvasContainer from './CanvasContainer'
 
 describe('Canvas', () => {
   it('has rendered correctly with expected initial state', () => {
@@ -23,6 +24,30 @@ describe('Canvas', () => {
 
     const canvas = screen.getByTestId('canvas')
     expect(canvas).toBeInTheDocument()
+    expect(draw).toHaveBeenCalled()
+  })
+})
+
+describe('CanvasContainer', () => {
+  it('has rendered correctly with expected initial state', () => {
+    const draw = jest.fn()
+    const options: CanvasOption = { contextId: '2d' }
+
+    render(
+      <CanvasContainer
+        draw={draw}
+        height={300}
+        width={300}
+        options={options}
+        id={'canvas'}
+        label={'Canvas Label'}
+      />,
+    )
+
+    const canvas = screen.getByTestId('canvas')
+    expect(canvas).toBeInTheDocument()
+    const label = screen.getByText('Canvas Label')
+    expect(label).toBeInTheDocument()
     expect(draw).toHaveBeenCalled()
   })
 })
