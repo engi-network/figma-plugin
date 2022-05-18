a34dfa5d55242a12fefcada523c5c8a0bfc4dd2c
Dream High <smartcoder0415@gmail.com>
2022-05-18 14:18:13 -0400

    added mock story for stepper

---
+++ src/app/components/global/Stepper/Stepper.stories.tsx
@@ -0,0 +1,14 @@
+import Stepper from './Stepper'
+
+export default {
+  component: Stepper,
+  title: 'Global/Components/Stepper',
+}
+
+export function StepperWithKnobs() {
+  return (
+    <div>
+      <Stepper />
+    </div>
+  )
+}
