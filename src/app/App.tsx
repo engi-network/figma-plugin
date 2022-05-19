e02f5c18f0417ea70c43b238051d2fed354286f0
Dream High <smartcoder0415@gmail.com>
2022-05-19 08:51:55 -0400

    removed unnessary react-query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
 import './styles/global.css'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 import { MemoryRouter as Router } from 'react-router-dom'
 
 import Layout from '~/app/components/modules/App/Layout/Layout'
@@ -11,30 +9,25 @@ import Routes from '~/app/pages/Routes'
 
 import { ROUTES, ROUTES_MAP } from './lib/constants'
 
-const queryClient = new QueryClient()
-
 function App() {
   return (
-    <AppContextProvider>
-      <UserContextProvider>
-        <QueryClientProvider client={queryClient}>
-          <Router
-            initialEntries={[
-              ROUTES_MAP[ROUTES.HOME],
-              ROUTES_MAP[ROUTES.RESULT],
-              ROUTES_MAP[ROUTES.HISTORY],
-              ROUTES_MAP[ROUTES.LOADING],
-            ]}
-            initialIndex={0}
-          >
-            <Layout>
-              <Routes />
-            </Layout>
-          </Router>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </UserContextProvider>
-    </AppContextProvider>
+    <Router
+      initialEntries={[
+        ROUTES_MAP[ROUTES.HOME],
+        ROUTES_MAP[ROUTES.RESULT],
+        ROUTES_MAP[ROUTES.HISTORY],
+        ROUTES_MAP[ROUTES.LOADING],
+      ]}
+      initialIndex={0}
+    >
+      <AppContextProvider>
+        <UserContextProvider>
+          <Layout>
+            <Routes />
+          </Layout>
+        </UserContextProvider>
+      </AppContextProvider>
+    </Router>
   )
 }
 
