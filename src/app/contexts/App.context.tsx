394a235a537582937a26ff0ad0b4500add48bb6e
Dream High <smartcoder0415@gmail.com>
2022-05-25 12:46:27 -0400

    cleaned up AppContext

--- src/app/contexts/App.context.tsx
+++ src/app/contexts/App.context.tsx
@@ -1,5 +1,4 @@
 import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router'
 import { useLocation } from 'react-router-dom'
 
 import { QueryState } from '~/app/@types/route'
@@ -17,8 +16,6 @@ import {
   STATUS,
 } from '~/app/models/Report'
 import { SAME_STORY_HISTORY_CREATE_FROM_UI_TO_PLUGIN } from '~/plugin/constants'
-
-import { ROUTES, ROUTES_MAP } from '../lib/constants'
 
 export interface AppContextProps {
   globalError: string
@@ -39,7 +36,6 @@ export function useAppContextSetup(): AppContextProps {
   const [globalError, setGlobalError] = useState('')
   const [numberOfInProgress, setNumberOfInProgress] = useState(0)
   const location = useLocation()
-  const navigate = useNavigate()
 
   const state = (location.state as QueryState) ?? {}
   const checkId = (state as Record<string, string>).checkId as unknown as string
@@ -72,11 +68,6 @@ export function useAppContextSetup(): AppContextProps {
         data: detailedReport,
       })
       setReport(detailedReport)
-
-      // if this is the current one, redirect to the result page
-      // if (checkId === check_id) {
-      //   navigate(ROUTES_MAP[ROUTES.RESULT])
-      // }
 
       SocketManager.terminateById(check_id, 1000, 'Successfully closed')
       return
@@ -111,11 +102,6 @@ export function useAppContextSetup(): AppContextProps {
         tagData: { check_id },
       })
 
-      // if (checkId === check_id) {
-      //   setGlobalError('Something went wrong. Please double check the inputs.')
-      //   navigate(ROUTES_MAP[ROUTES.ERROR])
-      // }
-
       SocketManager.terminateById(
         check_id,
         1000,
@@ -123,12 +109,9 @@ export function useAppContextSetup(): AppContextProps {
       )
       return
     }
-
-    // when in progress
   }
 
   // update subscribe when history and check id change
-
   useEffect(() => {
     SocketManager.wsList?.forEach((ws) => {
       ws.wsHandler.updateSubscribe('wsCallback', wsCallback)
