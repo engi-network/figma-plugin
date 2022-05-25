789a1f36443c46cd5de78575b47324b553d1a1c7
Dream High <smartcoder0415@gmail.com>
2022-05-25 14:31:04 -0400

    changed stepper styles

--- src/app/components/global/Stepper/Step.tsx
+++ src/app/components/global/Stepper/Step.tsx
@@ -12,20 +12,17 @@ function Step({ children, step, isLast, activeStep }: StepperItemProps) {
   const bgColor = isPassed ? 'bg-primary-white' : 'bg-[#ffffff4d]'
 
   return (
-    <li className={`relative  ${isLast ? '' : 'h-20'}`}>
-      {!isLast && (
-        <div
-          className="absolute inset-0 flex justify-center "
-          aria-hidden="true"
-        >
-          <div className={`h-full w-0.5 ${bgColor}`}></div>
-        </div>
-      )}
+    <li className={`relative flex flex-col ${isLast ? '' : 'h-20'}`}>
       <div
         className={`relative flex items-center justify-center ${bgColor} rounded-full p-2`}
       >
         {children}
       </div>
+      {!isLast && (
+        <div className="flex justify-center flex-1" aria-hidden="true">
+          <div className={`h-full w-0.5 ${bgColor}`}></div>
+        </div>
+      )}
     </li>
   )
 }
