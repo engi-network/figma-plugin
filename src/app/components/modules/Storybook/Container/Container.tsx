ca91cc3f1a04a879b0a6676b7a57afc71b59ce7c
Dream High <smartcoder0415@gmail.com>
2022-05-31 20:54:53 -0400

    added Container only for Storybooks

--- src/app/components/modules/Storybook/Container/Container.tsx
+++ src/app/components/modules/Storybook/Container/Container.tsx
@@ -1,15 +1,18 @@
+import cn from 'classnames'
 import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
+  className?: string
 }
 
-function Container({ children }: Props) {
-  return (
-    <div className="h-[100vh] w-full bg-slate-800 flex justify-center items-center container mx-auto p-10">
-      {children}
-    </div>
+function Container({ children, className }: Props) {
+  const rootClasses = cn(
+    className,
+    'h-[100vh] w-full bg-slate-800 flex justify-center items-center container mx-auto p-10',
   )
+
+  return <div className={rootClasses}>{children}</div>
 }
 
 export default Container
