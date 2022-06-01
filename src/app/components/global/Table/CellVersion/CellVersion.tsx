bdf6700d70c247db9ecb7b9adf989cf5dbbad6db
Dream High <smartcoder0415@gmail.com>
2022-06-01 10:09:11 -0400

    changed Cellversion font

--- src/app/components/global/Table/CellVersion/CellVersion.tsx
+++ src/app/components/global/Table/CellVersion/CellVersion.tsx
@@ -8,10 +8,10 @@ interface Props {
 function CellVersion({ value: { commit, branch } }: Props) {
   return (
     <div className="w-full overflow-hidden">
-      <h4 className="text-text-secondary font-bold leading-6 overflow-hidden truncate">
+      <h4 className="text-text-primary text-sm font-bold leading-6 overflow-hidden truncate">
         {branch}
       </h4>
-      <p className="m-0 text-text-secondary text-sm leading-6 overflow-hidden truncate">
+      <p className="m-0 text-text-secondary text-xs leading-6 overflow-hidden truncate">
         {commit}
       </p>
     </div>
