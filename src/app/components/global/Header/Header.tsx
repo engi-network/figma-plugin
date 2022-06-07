14a968fd77c723ae0caca27c17326e4383699670
Dream High <smartcoder0415@gmail.com>
2022-06-07 15:20:43 -0400

    changed logic

--- src/app/components/global/Header/Header.tsx
+++ src/app/components/global/Header/Header.tsx
@@ -76,7 +76,7 @@ function Header({ numberOfProgress = 0, setHistory }: Props) {
   return (
     <header className="flex justify-between border-b border-text-secondary px-7 py-5">
       <div className="flex">
-        {!numberOfProgress && (
+        {numberOfProgress <= 0 ? (
           <IconButton
             className="text-text-secondary mr-6"
             icon={<HistoryIcon className="text-text-secondary w-5 h-5" />}
@@ -84,26 +84,25 @@ function Header({ numberOfProgress = 0, setHistory }: Props) {
           >
             {ui('main.history')}
           </IconButton>
-        )}
-        <div
-          className="flex justify-center items-center"
-          onClick={handleViewProgress}
-          tabIndex={-1}
-          role="button"
-          aria-label="In progress"
-        >
-          <IconButton
-            className="text-text-secondary mr-2"
-            icon={<ClockIcon className="text-text-secondary w-5 h-5" />}
+        ) : (
+          <div
+            className="flex justify-center items-center"
+            onClick={handleViewProgress}
+            tabIndex={-1}
+            role="button"
+            aria-label="In progress"
           >
-            {ui('header.inProgress')}
-          </IconButton>
-          {!!numberOfProgress && (
+            <IconButton
+              className="text-text-secondary mr-2"
+              icon={<ClockIcon className="text-text-secondary w-5 h-5" />}
+            >
+              {ui('header.inProgress')}
+            </IconButton>
             <span className="text-primary-green font-bold bg-black/40 backdrop-blur-[4px] rounded-full text-xs flex-inline justify-center items-center w-4 h-4 pl-[5px]">
               {numberOfProgress}
             </span>
-          )}
-        </div>
+          </div>
+        )}
         {isDev && (
           <IconButton
             className="text-text-secondary ml-2"
