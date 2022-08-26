acbf711db424ad01f545af31905eeab8db289968
Dream High <smartcoder0415@gmail.com>
2022-08-26 12:44:33 -0400

    updated codeblock story

--- src/app/components/global/CodeBlock/CodeBlock.stories.tsx
+++ src/app/components/global/CodeBlock/CodeBlock.stories.tsx
@@ -1,26 +1,26 @@
-import { boolean } from '@storybook/addon-knobs'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import CodeBlock from './CodeBlock'
 
 export default {
   component: CodeBlock,
   title: 'Global/Components/CodeBlock',
-}
+} as ComponentMeta<typeof CodeBlock>
 
 const codeString = `const TestCompenent = () => {
     return <div>Test</div>
   }
 `
-export function CodeBlockStory() {
-  const showLineNumbers = boolean('Show lineNumber?', true)
 
-  return (
-    <div className="bg-slate-800 h-full p-10" id="code-block-test">
-      <CodeBlock
-        codeString={codeString}
-        className={'w-full h-20'}
-        showLineNumbers={showLineNumbers}
-      />
-    </div>
-  )
+const Template: ComponentStory<typeof CodeBlock> = (args) => (
+  <div className="bg-slate-800 h-full p-10" id="code-block-test">
+    <CodeBlock {...args} />
+  </div>
+)
+
+export const CodeBlockStory = Template.bind({})
+CodeBlockStory.args = {
+  codeString,
+  className: 'w-full h-20',
+  showLineNumbers: true,
 }
