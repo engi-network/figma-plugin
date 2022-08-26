fd1db40567aa1e6a7f3c5270169ed90df432f827
Dream High <smartcoder0415@gmail.com>
2022-08-26 12:39:52 -0400

    updated Checkbox

--- src/app/components/global/Checkbox/Checkbox.stories.tsx
+++ src/app/components/global/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import { boolean, text } from '@storybook/addon-knobs'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { useState } from 'react'
 
 import Checkbox from './Checkbox'
@@ -6,9 +6,9 @@ import Checkbox from './Checkbox'
 export default {
   component: Checkbox,
   title: 'Global/Components/Checkbox',
-}
+} as ComponentMeta<typeof Checkbox>
 
-export function CheckboxWithKnobs() {
+const Template: ComponentStory<typeof Checkbox> = (args) => {
   const [value, setValue] = useState<boolean>(false)
   const handleChange = (value?: boolean | string) => {
     setValue(!!value)
@@ -16,12 +16,15 @@ export function CheckboxWithKnobs() {
 
   return (
     <div className="bg-slate-700 h-screen">
-      <Checkbox
-        checked={value}
-        onChange={handleChange}
-        isDisabled={boolean('Disabled?', false)}
-        label={text('Checkbox Label', 'Check me')}
-      />
+      <Checkbox checked={value} onChange={handleChange} {...args} />
     </div>
   )
 }
+export const CheckboxStory = Template.bind({})
+
+CheckboxStory.args = {
+  isDisabled: false,
+  label: 'Check me',
+}
+
+export function CheckboxWithKnobs() {}
