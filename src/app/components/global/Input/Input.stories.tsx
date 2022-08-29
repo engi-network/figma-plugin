b4e00c2a09a6cda1890ff6900215f36765b13094
Dream High <smartcoder0415@gmail.com>
2022-08-29 09:20:09 -0400

    updated Input & Popover

--- src/app/components/global/Input/Input.stories.tsx
+++ src/app/components/global/Input/Input.stories.tsx
@@ -1,4 +1,4 @@
-import { boolean, text } from '@storybook/addon-knobs'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { useState } from 'react'
 
 import Input from './Input'
@@ -6,25 +6,25 @@ import Input from './Input'
 export default {
   component: Input,
   title: 'Global/Components/Input',
-}
+} as ComponentMeta<typeof Input>
 
-export function InputWithKnobs() {
+const Template: ComponentStory<typeof Input> = (args) => {
   const [value, setValue] = useState('')
   const handleChangeValue = (value: string) => {
     setValue(value)
   }
-  const isDisabled = boolean('Disabled?', false)
 
   return (
     <div className="bg-primary-white">
-      <Input
-        label="Component"
-        onChange={handleChangeValue}
-        value={value}
-        placeholder={text('Placehoder', 'placehoder')}
-        error={text('Error', 'This field has an error!')}
-        disabled={isDisabled}
-      />
+      <Input value={value} {...args} onChange={handleChangeValue} />
     </div>
   )
+}
+
+export const InputStory = Template.bind({})
+InputStory.args = {
+  label: 'Component',
+  placeholder: 'Placeholder',
+  error: 'This field has an error',
+  disabled: false,
 }
--- src/app/components/global/Popover/Popover.stories.tsx
+++ src/app/components/global/Popover/Popover.stories.tsx
@@ -1,16 +1,23 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
 import Popover from './Popover'
 
 export default {
   component: Popover,
   title: 'Global/Components/Popover',
-}
+} as ComponentMeta<typeof Popover>
 
-export function PopoverWithKnobs() {
-  return (
-    <div className="h-64 bg-slate-700">
-      <Popover title={'Filter by'} panelClassName="w-64">
-        <div>popover content</div>
-      </Popover>
-    </div>
-  )
+const Template: ComponentStory<typeof Popover> = (args) => (
+  <div className="h-64 bg-slate-700">
+    <Popover {...args}>
+      <>{args.children}</>
+    </Popover>
+  </div>
+)
+
+export const PopoverStory = Template.bind({})
+PopoverStory.args = {
+  children: <div>popover content</div>,
+  title: 'Filter by',
+  panelClassName: 'w-64',
 }
